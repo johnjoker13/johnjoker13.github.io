d7ec7bfbb2dced1c78c860aa689c476e365e63e7
joaoOliveira <joaoavelino2008@hotmail.com>
2022-05-19 17:48:24 -0300

    refactor(script.js): only one element can be selected

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 const li = document.querySelectorAll('.container__list__item');
 const btn = document.querySelector('.container__btn');
 
-for (let i = 0; i < li.length; i+= 1) {
-  li[i].addEventListener('click', () => {
-    btn.classList.toggle('selected');
+let selectedOne;
+btn.disabled = true;
+
+li.forEach((x) => {
+  x.addEventListener('click', () => {
+    selectedOne && selectedOne.classList.remove('selected');
+    selectedOne = x;
+    selectedOne.classList.add('selected');
+    btn.classList.add('selected');
+    btn.disabled = false;
   });
-};
+});
+
+btn.addEventListener('click', () => console.log('clicked'));
